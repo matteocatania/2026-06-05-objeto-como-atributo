70c1655ce7635fe5b71bfe28f55389d4cd283b14
matteocatania <t.catania@alumno.etec.um.edu.ar>
2026-06-12 10:38:24 -0300

    tarea 12/6

---
+++ app/src/main/Main.kt
@@ -0,0 +1,30 @@
+package org.example
+import org.example.Persona
+import org.example.Fecha
+import org.example.Polinomio
+import org.example.Cuadratica
+import org.example.CuadraticaMejor
+fun main() {
+    println(Persona("pepe", "honguito", Fecha(5, 6, 2026)))
+    val polinomio: Polinomio = Polinomio()
+    println(polinomio)
+    val cuadratica: Cuadratica = Cuadratica()
+    println (cuadratica)
+    val cuadraticamejor: CuadraticaMejor= CuadraticaMejor()
+    println (cuadraticamejor)
+    var objeto:Polinomio
+    objeto=Cuadratica()
+    println(objeto)
+    objeto=CuadraticaMejor()
+    println(objeto)
+    var lista = ArrayList <Polinomio>()
+    lista.add(Cuadratica())
+    lista.add(Polinomio())
+    lista.add(CuadraticaMejor())
+    for (var P : lista){
+        println(p)
+    }
+
+
+
+}
---
+++ app/src/main/kotlin/org/example/Cuadratica.kt
@@ -0,0 +1,11 @@
+ Cuadratica
+[10:34]package org.example
+import org.example.Polinomio
+open class Cuadratica:Polinomio(){
+        override fun toString(): String{
+        return ("Soy una cuadratica")
+    }
+
+
+
+}
